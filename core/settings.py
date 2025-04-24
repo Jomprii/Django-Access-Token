9b316fc4ad8d07898cff7a530bf922f69d9db63e
Jomprii <jyamson30@gmail.com>
2025-04-25 04:18:14 +0800

    Django Access Token

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+5y5y%_z#3b2vv#w$u+h)(_8w3kwsf=n^u7o2ya!^9)3cb0fs0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://django-access-token.onrender.com"]
 
 
 # Application definition
